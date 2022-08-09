16caf15f7018fc1db4dce9112a64ba1663a287d7
UYoung <wawa634@gmail.com>
2022-08-09 16:02:53 +0900

    commit

---
+++ musik/lib/view/song_detail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SongDetail extends StatefulWidget {
+  const SongDetail({Key? key}) : super(key: key);
+
+  @override
+  State<SongDetail> createState() => _SongDetailState();
+}
+
+class _SongDetailState extends State<SongDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
