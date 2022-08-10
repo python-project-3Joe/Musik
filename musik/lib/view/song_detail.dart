455c2008e7906db2dcb3bf31f9bfb2d0deac2d2f
Sana <hodoo0525@gmail.com>
2022-08-10 12:06:02 +0900

    기능 업데이트

--- musik/lib/view/song_detail.dart
+++ musik/lib/view/song_detail.dart
@@ -50,103 +50,107 @@ class _SongDetailState extends State<SongDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("$songname"),
-        backgroundColor: Colors.white,
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                child: YoutubePlayer(controller: controller),
-              ),
-              Container(
-                width: 400,
-                height: 500,
-                child: Column(
-                  children: [
-                    Card(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Column(
-                            children: [
-                              Image.network(
-                                // 이미지 url
-                                "$image",
-                                width: 80,
-                                height: 80,
-                                fit: BoxFit.fill,
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Column(
-                                children: [
-                                  Text("$songname") // 노래 제목
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Column(
-                                children: [
-                                  Text("$singer") // 가수 이름
-                                ],
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 80,
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [Text("$date")],
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    Card(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Column(
-                                children: [
-                                  Text("가사\n\n") // 노래 제목
-                                ],
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                '$lyrics',
-                                style: const TextStyle(fontSize: 15),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+    return GestureDetector(onTap: (){
+      FocusScope.of(context).unfocus();
+    },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("$songname"),
+          backgroundColor: Colors.white,
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: YoutubePlayer(controller: controller),
                 ),
-              ),
-            ],
+                Container(
+                  width: 400,
+                  height: 500,
+                  child: Column(
+                    children: [
+                      Card(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Column(
+                              children: [
+                                Image.network(
+                                  // 이미지 url
+                                  "$image",
+                                  width: 80,
+                                  height: 80,
+                                  fit: BoxFit.fill,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 30,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text("$songname") // 노래 제목
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                Column(
+                                  children: [
+                                    Text("$singer") // 가수 이름
+                                  ],
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 80,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [Text("$date")],
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      Card(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text("가사\n\n") // 노래 제목
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  '$lyrics',
+                                  style: const TextStyle(fontSize: 15),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
